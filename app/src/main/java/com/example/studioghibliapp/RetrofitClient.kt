d23ca110bd5d56a1da45b1f03909b4618310d465
Tomas Silvestre <silvestre@eudaimonia.com.ar>
2022-10-10 18:30:29 -0300

    added connection with studio ghibli API. added films to list. added film's details

---
+++ app/src/main/java/com/example/studioghibliapp/RetrofitClient.kt
@@ -0,0 +1,20 @@
+package com.example.studioghibliapp
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitClient {
+    private const val baseURL = "https://ghibliapi.herokuapp.com"
+
+    private val moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseURL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+}
